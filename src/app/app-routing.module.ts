3448a00ec7041a8ef3175d739c0c5461824f0b56
j_and <j_andrespuk@hotmail.com>
2020-09-22 12:52:02 +0300

    RouteGuards. ALSO LEARN GIT PROPERLY!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,14 @@ import {ErrorComponent} from './error/error.component';
 import {ListTodosComponent} from './list-todos/list-todos.component';
 import {MenuComponent} from './menu/menu.component';
 import {LogoutComponent} from './logout/logout.component';
+import {RouteGuardService} from './service/route-guard.service';
 
 const routes: Routes = [
   // welcome
   { path: '', component: LoginComponent}, // default path
   { path: 'login', component: LoginComponent},
-  { path: 'welcome/:name', component: WelcomeComponent},
-  { path: 'todos', component: ListTodosComponent},
+  { path: 'welcome/:name', component: WelcomeComponent, canActivate: [RouteGuardService]}, // canActivate takes an array of services
+  { path: 'todos', component: ListTodosComponent, canActivate: [RouteGuardService]},
   { path: 'logout', component: LogoutComponent},
   { path: '**', component: ErrorComponent} // ** for anything else. NB! This has to be the last route
 
---
+++ src/app/service/route-guard.service.ts
@@ -0,0 +1,22 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
+import {HardcodedAuthenticationService} from './hardcoded-authentication.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RouteGuardService implements CanActivate {
+  // Can't use without CanActivate interface
+
+  constructor(private hardCodedAuthenticationService: HardcodedAuthenticationService, private router: Router) {
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    if (this.hardCodedAuthenticationService.isUserLoggedIn()) {
+      return true;
+    } else {
+      this.router.navigate(['login']);
+      return false;
+    }
+  }
+}
